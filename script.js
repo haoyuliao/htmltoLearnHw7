c8abc6c445881c41211d5ea5e7401c096b2797fc
Ｈowie <Howie@owiede-MacBook-Pro.local>
2016-11-02 17:40:18 +0800

    no message

---
+++ script.js
@@ -0,0 +1,36 @@
+function random(num) {
+    console.log(num);
+    return 1+Math.floor(num * Math.random());
+}
+
+function max(n1, n2) {
+    if(n1 > n2) {
+        console.log(n1);
+        return n1
+    } else {
+        console.log(n2);
+        return n2
+    }
+}
+
+function lasrChar(arr) {
+    console.log(arr);
+    return arr[arr.length-1];
+}
+
+function squares(a, b) {
+    console.log(a);
+    console.log(b);
+    return a**2+b**2
+}
+
+function leapYear(y) {
+    console.log(y);
+    if(y%4 === 0 && y%100 > 0) {
+        return "yes"
+    } else if(y%400 === 0) {
+        return "yes"
+    } else {
+        return "no"
+    }   
+}
